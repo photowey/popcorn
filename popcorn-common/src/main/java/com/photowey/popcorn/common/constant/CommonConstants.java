3e39b9fe4e11bef8b024cf308124d02ee3472dbf
photowey <photowey@gmail.com>
2023-07-26 22:36:17 +0800

    feat: Add: 1.constant variable.

---
+++ popcorn-common/src/main/java/com/photowey/popcorn/common/constant/CommonConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.popcorn.common.constant;
+
+/**
+ * {@code CommonConstants}
+ *
+ * @author photowey
+ * @date 2023/07/26
+ * @since 1.0.0
+ */
+public interface CommonConstants {
+
+    // --------------------------------------------------------- String
+
+    String EMPTY_STRING = "";
+    String SPACE_STRING = " ";
+
+    // --------------------------------------------------------- Long
+
+    long MILLIS_UNIT = 1000L;
+    long TIME_STAMP_LENGTH = 13;
+}
---
+++ popcorn-common/src/main/java/com/photowey/popcorn/common/date/DatePatternConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.photowey.popcorn.common.date;
+
+/**
+ * {@code DatePatternConstants}
+ *
+ * @author photowey
+ * @date 2023/07/26
+ * @since 1.0.0
+ */
+public interface DatePatternConstants {
+
+    String yyyy_MM_dd = "yyyy-MM-dd";
+    String HH_mm_ss = "HH:mm:ss";
+
+    String yyyyMMdd = "yyyyMMdd";
+    String yyyyMM = "yyyyMM";
+    String yyMM = "yyMM";
+    String yyMMdd = "yyMMdd";
+    String HHmmss = "HHmmss";
+
+    String yyyy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
+    String yyyy_MM_dd_HH_mm_ss_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+
+    String MMddHHmmss = "MMddHHmmss";
+    String yyMMddHHmmss = "yyMMddHHmmss";
+    String yyyyMMddHHmmss = "yyyyMMddHHmmss";
+    String yyyyMMddHHmmssSSS = "yyyyMMddHHmmssSSS";
+
+}
